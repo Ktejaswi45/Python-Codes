1118771a1fddbc9523d0e438c66b61fb2c41c67f
Tejaswi Kruthiventi <142220578+Ktejaswi45@users.noreply.github.com>
2025-01-10 22:36:32 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,6 @@
+#You are given 3 integer inputs a,b,c in first line d,e in second line we need to get sum of all the integers
+
+a,b,c= map(int,input().split())
+d,e= map(int,input().split())
+
+print(a+b+c+d+e)
