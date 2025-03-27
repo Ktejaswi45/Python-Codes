643362733afece5e44ce73321cef071ed8ae0a96
Tejaswi Kruthiventi <142220578+Ktejaswi45@users.noreply.github.com>
2025-03-27 20:15:58 +0530

    Add files via upload

---
+++ day74.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range (int(input())):
+    a,b,c=map(int,input().split())
+    if a>50:
+        print('a')
+    elif b>50:
+        print('b')
+    elif c>50:
+        print('c')
+    else:
+        print('nota')
