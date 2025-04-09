7f11782f0ac8212ba7a37240c38012b455d846f9
Tejaswi Kruthiventi <142220578+Ktejaswi45@users.noreply.github.com>
2025-04-09 21:13:07 +0530

    Add files via upload

---
+++ day87.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range (int(input())):
+    n=int(input())
+    a=input()
+    if 'I' in a:
+        print("INDIAN")
+    elif 'Y' in a:
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
